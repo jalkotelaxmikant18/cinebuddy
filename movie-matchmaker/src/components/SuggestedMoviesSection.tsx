e5cf5ab544076a243c21a68a934f34b15bbffc0b
jalkotelaxmikant18 <jalkotelaxmikant018@gmail.com>
2026-05-10 23:45:20 +0530

    SuggestedMoviesSection.tsx

--- movie-matchmaker/src/components/SuggestedMoviesSection.tsx
+++ movie-matchmaker/src/components/SuggestedMoviesSection.tsx
@@ -154,7 +154,7 @@ const SuggestedMoviesSection = ({ isLoggedIn }) => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
           {suggestedMovies.map((movie, index) => (
             <motion.div
               key={movie.id}
